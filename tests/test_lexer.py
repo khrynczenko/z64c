67cee3b79d20f42af339c471ecb94bb9af9020fb
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-06-21 20:28:52 +0200

    feat(lexer): add basic tokenizer

---
+++ tests/test_lexer.py
@@ -0,0 +1,80 @@
+from z64c import lexer as lex
+
+
+def test_tokenizer_produces_integer():
+    source = "  123  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.INTEGER, "123")]
+
+
+def test_tokenizer_produces_star():
+    source = "  *  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.STAR, "*")]
+
+
+def test_tokenizer_produces_slash():
+    source = "  /  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.SLASH, "/")]
+
+
+def test_tokenizer_produces_plus():
+    source = "  +  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.PLUS, "+")]
+
+
+def test_tokenizer_produces_minus():
+    source = "  -  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.MINUS, "-")]
+
+
+def test_tokenizer_produces_equal():
+    source = "  =  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.EQUAL, "=")]
+
+
+def test_tokenizer_produces_newline():
+    source = "  \n  "
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [lex.Token(1, 3, lex.Category.NEWLINE, "\n")]
+
+
+def test_tokenizer_produces_token_afte_a_newline():
+    source = "  \n123"
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [
+        lex.Token(1, 3, lex.Category.NEWLINE, "\n"),
+        lex.Token(2, 1, lex.Category.INTEGER, "123"),
+    ]
+
+
+def test_tokenizer_produces_binary_addition():
+    source = "  12 + 34"
+    tokenizer = lex.Tokenizer(source)
+    tokens = tokenizer.tokenize()
+
+    assert tokens == [
+        lex.Token(1, 3, lex.Category.INTEGER, "12"),
+        lex.Token(1, 6, lex.Category.PLUS, "+"),
+        lex.Token(1, 8, lex.Category.INTEGER, "34"),
+    ]
---
+++ z64c/lexer.py
@@ -0,0 +1,126 @@
+import dataclasses
+import enum
+import itertools
+
+from typing import Text, List
+
+
+@enum.unique
+class Category(enum.Enum):
+    EOF = enum.auto()
+    NEWLINE = enum.auto()
+
+    STAR = enum.auto()
+    SLASH = enum.auto()
+    PLUS = enum.auto()
+    MINUS = enum.auto()
+    EQUAL = enum.auto()
+
+    INTEGER = enum.auto()
+
+
+@dataclasses.dataclass
+class Token:
+    line: int
+    column: int
+    category: Category
+    lexeme: str
+
+
+class Tokenizer:
+    def __init__(self, source: Text):
+        self._source = source
+        self._source_index = 0
+        self._line = 1
+        self._column = 1
+        self._produced_tokens = []
+
+    def tokenize(self) -> List[Token]:
+        while self._source_index < len(self._source):
+            if self._source[self._source_index] == " ":
+                self._advance()
+            elif self._source[self._source_index] == "\n":
+                self._produced_tokens.append(self._consume_newline())
+
+            elif self._source[self._source_index].isdigit():
+                self._produced_tokens.append(self._consume_integer())
+
+            elif self._source[self._source_index] == "*":
+                self._produced_tokens.append(self._consume_star())
+
+            elif self._source[self._source_index] == "/":
+                self._produced_tokens.append(self._consume_slash())
+
+            elif self._source[self._source_index] == "+":
+                self._produced_tokens.append(self._consume_plus())
+
+            elif self._source[self._source_index] == "-":
+                self._produced_tokens.append(self._consume_minus())
+
+            elif self._source[self._source_index] == "=":
+                self._produced_tokens.append(self._consume_equal())
+
+            else:
+                raise RuntimeError(
+                    f"Unrecognized syntax at line {self._line} "
+                    "column {self._column}."
+                )
+
+        return self._produced_tokens
+
+    def _advance(self):
+        if self._source[self._source_index] == "\n":
+            self._line += 1
+            self._column = 0
+
+        self._column += 1
+        self._source_index += 1
+
+    def _consume_newline(self):
+        token = Token(self._line, self._column, Category.NEWLINE, "\n")
+        self._advance()
+
+        return token
+
+    def _consume_star(self):
+        token = Token(self._line, self._column, Category.STAR, "*")
+        self._advance()
+
+        return token
+
+    def _consume_slash(self):
+        token = Token(self._line, self._column, Category.SLASH, "/")
+        self._advance()
+
+        return token
+
+    def _consume_plus(self):
+        token = Token(self._line, self._column, Category.PLUS, "+")
+        self._advance()
+
+        return token
+
+    def _consume_minus(self):
+        token = Token(self._line, self._column, Category.MINUS, "-")
+        self._advance()
+
+        return token
+
+    def _consume_equal(self):
+        token = Token(self._line, self._column, Category.EQUAL, "=")
+        self._advance()
+
+        return token
+
+    def _consume_integer(self):
+        integer = "".join(
+            itertools.takewhile(
+                lambda c: c.isdigit(), self._source[self._source_index :]
+            )
+        )
+
+        token = Token(self._line, self._column, Category.INTEGER, integer)
+
+        self._column += len(integer)
+        self._source_index += len(integer)
+        return token
