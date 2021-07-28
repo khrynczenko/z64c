764887a04b6f52a7947309a062bb9719ffd435c3
Krzysztof Hrynczenko <jeniopy@gmail.com>
2021-07-28 17:29:01 +0200

    WIP: commit to amend

--- z64c/ast.py
+++ z64c/ast.py
@@ -1,10 +1,30 @@
 from __future__ import annotations
 
-from typing import List
+import abc
+
+from typing import List, Tuple
+from abc import ABC
+
+INDENTATION = "    "
 
 
-class Ast:
-    pass
+class Environment:
+    def __init__(self):
+        self._variables = {}
+        self._idx = 0
+
+    def add_variable(self, name: str, value):
+        self._idx = self._idx + 1
+        self._variables[name] = self._idx
+
+    def get_variable(self, name: str) -> int:
+        return self._variables[name]
+
+
+class Ast(ABC):
+    @abc.abstractmethod
+    def emit(self, environment: Environment):
+        pass
 
 
 class Program(Ast):
@@ -14,6 +34,13 @@ class Program(Ast):
     def __eq__(self, rhs: Program) -> bool:
         return self._statements == rhs._statements
 
+    def emit(self, environment: Environment):
+        print(f"{INDENTATION}org &1000")
+        for statement in self._statements:
+            print("")
+            statement.emit(environment)
+        print(f"{INDENTATION}ret")
+
 
 class Print(Ast):
     def __init__(self, expression: Ast):
@@ -21,6 +48,10 @@ class Print(Ast):
 
     def __eq__(self, rhs: Print) -> bool:
         return self._expression == rhs._expression
+
+    def emit(self, environment: Environment):
+        self._expression.emit(environment)
+        print(f"{INDENTATION}call &BB5A")
 
 
 class Assignment(Ast):
@@ -31,6 +62,10 @@ class Assignment(Ast):
     def __eq__(self, rhs: Assignment) -> bool:
         return self._name == rhs._name and self._rhs == rhs._rhs
 
+    def emit(self, environment: Environment):
+        environment.add_variable(self._name)
+        print(f"{INDENTATION}push a")
+
 
 class Addition(Ast):
     def __init__(self, lhs: Ast, rhs: Ast):
@@ -40,6 +75,12 @@ class Addition(Ast):
     def __eq__(self, rhs: Addition) -> bool:
         return self._lhs == rhs._lhs and self._rhs == rhs._rhs
 
+    def emit(self, environment: Environment):
+        self._lhs.emit(environment)
+        print(f"{INDENTATION}ld b, a")
+        self._rhs.emit(environment)
+        print(f"{INDENTATION}adc a, b")
+
 
 class Negation(Ast):
     def __init__(self, expression: Ast):
@@ -47,6 +88,10 @@ class Negation(Ast):
 
     def __eq__(self, rhs: Negation) -> bool:
         return self._expression == rhs._expression
+
+    def emit(self, environment: Environment):
+        self._expression.emit(environment)
+        print(f"{INDENTATION}neg")
 
 
 class Identifier(Ast):
@@ -56,6 +101,9 @@ class Identifier(Ast):
     def __eq__(self, rhs: Identifier) -> bool:
         return self._value == rhs._value
 
+    def emit(self, environment: Environment):
+        idx
+
 
 class Unsignedint(Ast):
     def __init__(self, value: int):
@@ -63,3 +111,6 @@ class Unsignedint(Ast):
 
     def __eq__(self, rhs: Unsignedint) -> bool:
         return self._value == rhs._value
+
+    def emit(self, environment: Environment):
+        print(f"{INDENTATION}ld a, {self._value}")
--- z64c/main.py
+++ z64c/main.py
@@ -1,2 +1,21 @@
+import click
+
+from z64c.ast import Environment
+from z64c.scanner import Scanner
+from z64c.parser import Parser
+
+
+@click.command()
+@click.argument("source", type=str)
+def z64c(source: str):
+    with open(source, "r") as file:
+        source_text = file.read()
+    scanner = Scanner(source_text)
+    tokens = scanner.scan()
+    parser = Parser(tokens)
+    ast = parser.parse()
+    ast.emit(Environment())
+
+
 def main():
-    print("Hello world!")
+    z64c()
